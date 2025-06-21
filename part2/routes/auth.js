b313154e0fd0aecb4f1be8772e08dc1388bb80d1
Aya-s8 <a1898199@adelaide.edu.au>
2025-06-21 05:50:23 +0000

    [automatic commit] 2025-06-21 05:50:23 UTC 101.177.82.229

--- part2/routes/auth.js
+++ part2/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../model/db');
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
