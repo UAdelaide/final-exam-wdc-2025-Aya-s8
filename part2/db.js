d6227c87bdbd66a5ecd4377e2009813feca4d5fc
Aya-s8 <a1898199@adelaide.edu.au>
2025-06-21 05:25:02 +0000

    [automatic commit] 2025-06-21 05:25:02 UTC 101.177.82.229

--- part2/db.js
+++ part2/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'DogWalkService', // 或你实际使用的数据库名称
+  database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
