034338af56ba0f52fb420fc024bbcc42b644d5dc
Aya-s8 <a1898199@adelaide.edu.au>
2025-06-21 04:15:02 +0000

    [automatic commit] 2025-06-21 04:15:02 UTC 101.177.82.229

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '', // fill in your password
+  password: '', 
   database: 'DogWalkService',
 });
 
